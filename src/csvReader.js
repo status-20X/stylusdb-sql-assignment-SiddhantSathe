57900023aef596a04486c5d4dfa15b1b07aae66b
Siddhant Sathe <sathesiddhant21@gmail.com>
2024-05-04 13:40:37 +0530

    step-2 try2

--- src/csvReader.js
+++ src/csvReader.js
@@ -1,20 +1,41 @@
+// const fs = require('fs');
+// const csv = require('csv-parser');
+
+// function readCSV(filePath) {
+//     const results = [];
+
+//     return new Promise((resolve, reject) => {
+//         fs.createReadStream(filePath)
+//             .pipe(csv())
+//             .on('data', (data) => results.push(data))
+//             .on('end', () => {
+//                 resolve(results);
+//             })
+//             .on('error', (error) => {
+//                 reject(error);
+//             });
+//     });
+// }
+
+// module.exports = readCSV;
+
+
 const fs = require('fs');
 const csv = require('csv-parser');
 
 function readCSV(filePath) {
-    const results = [];
-
     return new Promise((resolve, reject) => {
+        const data = [];
         fs.createReadStream(filePath)
             .pipe(csv())
-            .on('data', (data) => results.push(data))
+            .on('data', (row) => {
+                data.push(row);
+            })
             .on('end', () => {
-                resolve(results);
+                resolve(data);
             })
             .on('error', (error) => {
                 reject(error);
             });
     });
 }
-
-module.exports = readCSV;
