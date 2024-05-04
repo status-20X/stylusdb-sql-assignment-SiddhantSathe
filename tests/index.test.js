b7e6efca1c66f430ee65dac0270463058e29ac25
Siddhant Sathe <sathesiddhant21@gmail.com>
2024-05-04 13:34:03 +0530

    step-2 completed

--- tests/index.test.js
+++ tests/index.test.js
@@ -5,7 +5,7 @@ test('Basic Jest Test', () => {
   const readCSV = require('../src/csvReader');
 
 test('Read CSV File', async () => {
-    const data = await readCSV('./sample.csv');
+    const data = await readCSV('sample.csv');
     expect(data.length).toBeGreaterThan(0);
     expect(data.length).toBe(3);
     expect(data[0].name).toBe('John');
